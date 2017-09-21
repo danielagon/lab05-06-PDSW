6b4aa58c4b4dc2d09d895205de2400c897e6a527
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2017-09-21 12:20:51 -0500

    Definicion clases de equivalencia registro de pacientes, se agregó una dependecia e implementacion de prueba

--- src/test/java/tests/ServiciosPacientesTest.java
+++ src/test/java/tests/ServiciosPacientesTest.java
@@ -5,6 +5,9 @@
  */
 package tests;
 
+import edu.eci.pdsw.samples.entities.Paciente;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosPacientes;
+import edu.eci.pdsw.samples.services.impl.ServiciosPacientesMock;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -12,6 +15,22 @@ import static org.junit.Assert.*;
 /**
  *
  * @author hcadavid
+ * 
+ * 
+ * CLASES DE EQUIVALENCIA REGISTRO NUEVO PACIENTE
+ * 
+ * 
+ *CE1: DESCRIPCION: El nuevo paciente ya esta registrado (cedula sea igual)
+ *     RESULTADOS: Excepcion
+ * 
+ *CE2: DESCRIPCION: cedula del paciente nula
+ *     RESULTADO: Excepcion
+ * 
+ *CE3 DESCRIPCION: tipo de identificacion nulo 
+ *    RESULTADO: Excepcion
+ * 
+ *
+ * 
  */
 public class ServiciosPacientesTest {
     
@@ -20,6 +39,17 @@ public class ServiciosPacientesTest {
     
     @Before
     public void setUp() {
+    }
+    
+    @Test
+    public void registroPaciente(){
+        ServiciosPacientesMock servicios= new ServiciosPacientesMock();
+        try{
+            servicios.registrarNuevoPaciente(new Paciente(77777,null, "Ricardo Pinto", java.sql.Date.valueOf("1956-05-01"), eps6));
+        }catch (ExcepcionServiciosPacientes e){
+            assertEquals(e.getMessage(),"El tipo de identificaión no es valido");
+        }
+        
     }
     
 }
