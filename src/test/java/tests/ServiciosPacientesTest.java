5d3872736f0c5d9022c70fa1db5f8c32e9c934e0
danielagon <daniela.gonzalezramirez@hotmail.com>
2017-09-21 12:04:51 -0500

    Clases de equivalencia e implementacion de una prueba

--- src/test/java/tests/ServiciosPacientesTest.java
+++ src/test/java/tests/ServiciosPacientesTest.java
@@ -5,6 +5,9 @@
  */
 package tests;
 
+import edu.eci.pdsw.samples.entities.Consulta;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosPacientes;
+import edu.eci.pdsw.samples.services.impl.ServiciosPacientesMock;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -12,6 +15,17 @@ import static org.junit.Assert.*;
 /**
  *
  * @author hcadavid
+ * 
+ * CLASES DE EQUIVALENCIA
+ * 
+ * CE1: DESCRIPCION: El paciente no se encentre registrado
+ *      RESULTADO: Excepcion
+ * 
+ * CE2: DESCRIPCION: El paciente no existe
+ *      RESULTADO: Excepcion
+ * 
+ * CE3: DESCRIPCION: El paciente existe
+ *      RESULTADO: El id de la consulta aumenta 1 y se agrega la consulta al paciente
  */
 public class ServiciosPacientesTest {
     
@@ -20,6 +34,16 @@ public class ServiciosPacientesTest {
     
     @Before
     public void setUp() {
+    }
+    
+    @Test
+    public void registroConsultas(){
+        ServiciosPacientesMock servicios=new ServiciosPacientesMock();
+        try{
+            servicios.agregarConsultaPaciente(0, "CC", new Consulta(java.sql.Date.valueOf("2000-01-01"), "Dolor de cabeza", 454));  
+        }catch(ExcepcionServiciosPacientes e){
+            assertEquals(e.getMessage(),"Paciente 0 no esta registrado");
+        }
     }
     
 }
